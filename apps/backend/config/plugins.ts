14bae304e4f0bd9b4a515c721b541b594821d989
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-02-03 22:57:22 +0100

    Stage (#12)
    * Development (#2)
    
    * feat: add nx with next & strapi
    
    * feat: update
    
    * Development (#5)
    
    * feat: add nx with next & strapi
    
    * feat: update
    
    * Feat/development submodule utils (#4)
    
    * feat: add submodiles
    
    * fix: delete package-lock.json
    
    * feat: add submodules
    
    * Development (#7)
    
    * feat: add nx with next & strapi
    
    * feat: update
    
    * Feat/development submodule utils (#4)
    
    * feat: add submodiles
    
    * fix: delete package-lock.json
    
    * feat: add submodules
    
    * Development (#8)
    
    * feat: add nx with next & strapi
    
    * feat: update
    
    * Feat/development submodule utils (#4)
    
    * feat: add submodiles
    
    * fix: delete package-lock.json
    
    * feat: add submodules

---
+++ apps/backend/config/plugins.ts
@@ -0,0 +1,40 @@
+module.exports = ({env}) => ({
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "hello@uxu.pl",
+        defaultReplyTo: "hello@uxu.pl",
+        testAddress: "hello@uxu.pl",
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 20,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
