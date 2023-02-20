c1b9eed94e1bab44ba229b5e2b06660738f576c2
Paweł Niedźwiecki <hello@uxu.pl>
2023-02-20 20:51:05 +0100

    feat: add basic config for apollo client

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,13 +1,27 @@
+import { Header } from '@uxu-utils';
+import { useQuery } from '@apollo/client';
+import { GET_SEARCH } from 'gql';
+import { GitBranch, GitHub } from 'react-feather';
 
 export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.styled-components file.
-   */
+  const { loading, error, data } = useQuery(GET_SEARCH, { variables: { query: 'testy' } });
+  console.log(data);
   return (
-    <p>po</p>
-  );
+    <>
+      <Header
+        res={{ data: [], query: '' }}
+        tabs={[
+          { title: 'GitLab', value: 'https://www.uxu.pl', active: true },
+          { title: 'GitHub', value: () => alert('UXU'), icon: <GitHub />, active: false },
+          { title: 'Bitbucket', value: 'https://www.uxu.pl', icon: <GitBranch />, active: false },
+        ]}
+        callBack={(search: string) => {
+          console.log({ query: search });
+        }}
+      />
+    </>
+  )
+    ;
 }
 
 export default Index;
