3a068cc405fc782ec8da0fc7e8a42c3cbdaede1b
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-03-13 23:14:54 +0100

    Feat/data search (#32)
    * feat: add hook for data search
    
    * feat: add hooks
    
    * fix: fix ts

--- apps/frontend/src/hooks/hook.search.ts
+++ apps/frontend/src/hooks/hook.search.ts
@@ -1,19 +1,42 @@
 import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_SEARCH } from '../gql';
+import { GET_SEARCH, GET_SEARCH_TYPE } from 'gql';
+import {
+  ResData,
+} from 'uxu-utils/libs/design-system/src/lib/components/organisms/form/search/component.search.types';
+import { throttle } from 'uxu-utils';
 
+const resInitialState: { data: ResData, query: string } = { data: [], query: '' };
 
 export const useHookSearch = () => {
   const [query, setQuery] = useState('');
-  const [res, setRes] = useState({ data: [], query });
-  const { loading, error, data, refetch } = useQuery(GET_SEARCH, { variables: { query: '' } });
+  const [res, setRes] = useState(resInitialState);
+  const { loading, error, data, refetch } = useQuery<GET_SEARCH_TYPE>(GET_SEARCH, { variables: { query: '' } });
 
   useEffect(() => {
-    refetch({ query });
+
+    throttle(() => {
+      console.log('ok');
+      refetch({ query });
+    }, 100);
   }, [query]);
 
-  console.log(data?.search?.articles?.data);
-  console.log(query);
 
-  return { setQuery };
+  useEffect(() => {
+    throttle(() => {
+      const newRes = resInitialState;
+      newRes.query = query;
+      data?.search?.articles?.data.map((art) => {
+        newRes.data.push({
+          title: art.attributes.title,
+          slug: '',
+          cover: null,
+        });
+      });
+      setRes(newRes);
+    }, 100);
+  }, [data, query]);
+
+
+  return { setQuery, res };
 };
