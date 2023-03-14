57c0709d5775188dec36b38b299bf71cc62d302e
Paweł Niedźwiecki <hello@uxu.pl>
2023-03-14 23:23:20 +0100

    fix: fix hook

--- apps/frontend/src/hooks/hook.search.ts
+++ apps/frontend/src/hooks/hook.search.ts
@@ -1,42 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_SEARCH, GET_SEARCH_TYPE } from 'gql';
-import {
-  ResData,
-} from 'uxu-utils/libs/design-system/src/lib/components/organisms/form/search/component.search.types';
-import { throttle } from 'uxu-utils';
+import {  useState } from 'react';
+// import { useQuery } from '@apollo/client';
+// import { GET_SEARCH, GET_SEARCH_TYPE } from 'gql';
+// import {
+//   ResData,
+// } from 'uxu-utils/libs/design-system/src/lib/components/organisms/form/search/component.search.types';
 
-const resInitialState: { data: ResData, query: string } = { data: [], query: '' };
+//
+// const resInitialState: { data: ResData, query: string } = { data: [], query: '' };
 
 export const useHookSearch = () => {
+  // let setThrottle = null;
   const [query, setQuery] = useState('');
-  const [res, setRes] = useState(resInitialState);
-  const { loading, error, data, refetch } = useQuery<GET_SEARCH_TYPE>(GET_SEARCH, { variables: { query: '' } });
-
-  useEffect(() => {
-
-    throttle(() => {
-      console.log('ok');
-      refetch({ query });
-    }, 100);
-  }, [query]);
+  // const [res, setRes] = useState(resInitialState);
+  // const { loading, error, data, refetch } = useQuery<GET_SEARCH_TYPE>(GET_SEARCH, { variables: { query: '' } });
 
 
-  useEffect(() => {
-    throttle(() => {
-      const newRes = resInitialState;
-      newRes.query = query;
-      data?.search?.articles?.data.map((art) => {
-        newRes.data.push({
-          title: art.attributes.title,
-          slug: '',
-          cover: null,
-        });
-      });
-      setRes(newRes);
-    }, 100);
-  }, [data, query]);
-
-
-  return { setQuery, res };
+  return { setQuery };
 };
