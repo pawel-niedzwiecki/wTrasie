6feb8a7921fa9531cef7b6fdfa6d041ef43393d6
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-03-14 23:24:51 +0100

    Development (#33)
    * feat: add nx with next & strapi
    
    * feat: update
    
    * Feat/development submodule utils (#4)
    
    * feat: add submodiles
    
    * fix: delete package-lock.json
    
    * feat: add submodules
    
    * fix: delete backend (#9)
    
    * fix: package.json (#10)
    
    * Frontend (#13)
    
    * fix: package.json
    
    * fix: update git submodule
    
    * update gitsubmodule
    
    * Feat/apolloclient (#16)
    
    * fix: package.json
    
    * fix: update git submodule
    
    * update gitsubmodule
    
    * fix: update gitsubmodule
    
    * feat: migrate
    
    * feat: add basic config for apollo client
    
    * Feat/apolloclient (#19)
    
    * fix: package.json
    
    * fix: update git submodule
    
    * update gitsubmodule
    
    * fix: update gitsubmodule
    
    * feat: migrate
    
    * feat: add basic config for apollo client
    
    * feat: add provider
    
    * feat: add query apollo client
    
    * fix: fix import
    
    * fix: fix submodule (#22)
    
    * fix: fix production ts (#25)
    
    * feat: add hooks
    
    * Feat/data search (#32)
    
    * feat: add hook for data search
    
    * feat: add hooks
    
    * fix: fix ts

--- apps/frontend/src/layout/layout.index.tsx
+++ apps/frontend/src/layout/layout.index.tsx
@@ -3,19 +3,18 @@ import { Headphones, Home, Tool } from 'react-feather';
 import { useHookSearch } from 'hooks';
 
 export const Layout = ({ children }: any) => {
-  const { setQuery } = useHookSearch();
+  const { setQuery, res } = useHookSearch();
 
   return (
     <>
       <Header
-        res={{ data: [], query: '' }}
+        res={res}
         tabs={[
           { title: 'Home', value: '/', icon: <Home />, active: true },
           { title: 'Usługi', value: '/uslugi', icon: <Tool />, active: false },
           { title: 'Kontakt', value: '/kontakt', icon: <Headphones />, active: false },
         ]}
         callBack={(search: string) => {
-          console.log(search);
           setQuery(search);
         }}
       />
