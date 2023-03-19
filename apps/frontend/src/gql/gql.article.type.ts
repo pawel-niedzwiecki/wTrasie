06f6d655ea7af347e5414c88a1990db846ab7179
Paweł Niedźwiecki <hello@uxu.pl>
2023-03-20 00:58:14 +0100

    fix: update search hooks

--- apps/frontend/src/gql/gql.article.type.ts
+++ apps/frontend/src/gql/gql.article.type.ts
@@ -1,4 +1,4 @@
-import { FRAGMENT_VIEWS_TYPE, FRAGMENT_TAGS_TYPE } from '.';
+import { FRAGMENT_VIEWS_TYPE, FRAGMENT_TAGS_TYPE , FRAGMENT_COVER} from '.';
 
 export type FRAGMENT_ARTICLES_TYPE = {
   __typename: 'ArticleEntityResponseCollection',
@@ -10,9 +10,17 @@ export type FRAGMENT_ARTICLE_TYPE = {
 }
 
 
+export type FRAGMENT_ARTICLE_LEAD_TYPE = {
+  __typename: 'ComponentContentPartsLead', id: string, lead: string | null
+} | null
+
+
 export type FRAGMENT_ATTRIBUTES_ARTICLE_TYPE = {
   __typename: 'Article',
   title: string,
+  type: string | null,
   tags: FRAGMENT_TAGS_TYPE,
-  views?: FRAGMENT_VIEWS_TYPE
+  views?: FRAGMENT_VIEWS_TYPE,
+  lead?: FRAGMENT_ARTICLE_LEAD_TYPE
+  cover?: FRAGMENT_COVER
 }
---
+++ apps/frontend/src/utils/function/createSlugForType.ts
@@ -0,0 +1,1 @@
+export const createSlugForType = (type: string) => type === 'article' ? 'a' : 's'
