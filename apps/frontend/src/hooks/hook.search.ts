a925f93f6e24c8706015beed30a3b9a0a3c75337
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-03-20 01:30:32 +0100

    Feat/data search (#34)
    * feat: add hook for data search
    
    * feat: add hooks
    
    * fix: fix ts
    
    * fix: fix hook
    
    * fix: update search hooks
    
    * feat: update

--- apps/frontend/src/hooks/hook.search.ts
+++ apps/frontend/src/hooks/hook.search.ts
@@ -4,39 +4,37 @@ import { GET_SEARCH, GET_SEARCH_TYPE } from 'gql';
 import {
   ResData,
 } from 'uxu-utils/libs/design-system/src/lib/components/organisms/form/search/component.search.types';
-import { throttle } from 'uxu-utils';
-
 const resInitialState: { data: ResData, query: string } = { data: [], query: '' };
+import { throttle, createSlug } from 'uxu-utils';
+import { createSlugForType } from 'utils';
 
 export const useHookSearch = () => {
-  const [query, setQuery] = useState('');
   const [res, setRes] = useState(resInitialState);
-  const { loading, error, data, refetch } = useQuery<GET_SEARCH_TYPE>(GET_SEARCH, { variables: { query: '' } });
+  const [query, setQuery] = useState('');
+  const { loading, data, refetch } = useQuery<GET_SEARCH_TYPE>(GET_SEARCH, { variables: { query: '' } });
 
+  useEffect(() => throttle(() => query.length && refetch({ query }), 400), [query]);
   useEffect(() => {
+    if (query?.length && data?.search?.articles?.data?.length) {
+      const resData = data?.search?.articles?.data.map((art) => {
+        const {
+          title,
+          type,
+          lead: { lead },
+          cover: { data: { attributes: { formats: { thumbnail: { url } } } } },
+        } = art.attributes;
 
-    throttle(() => {
-      console.log('ok');
-      refetch({ query });
-    }, 100);
-  }, [query]);
-
-
-  useEffect(() => {
-    throttle(() => {
-      const newRes = resInitialState;
-      newRes.query = query;
-      data?.search?.articles?.data.map((art) => {
-        newRes.data.push({
-          title: art.attributes.title,
-          slug: '',
-          cover: null,
-        });
+        return {
+          cover: url,
+          title: title,
+          excerpt: lead,
+          slug: `/${createSlugForType(type)}/${createSlug(title)}`,
+        };
       });
-      setRes(newRes);
-    }, 100);
+      setRes({ data: resData, query });
+    } else res !== resInitialState && setRes(resInitialState);
   }, [data, query]);
 
 
-  return { setQuery, res };
+  return { setQuery, res, loading };
 };
