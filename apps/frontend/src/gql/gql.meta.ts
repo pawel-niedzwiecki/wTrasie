10f077a7fde7730621c478de1d4cab2984261fb1
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-03-26 19:45:40 +0200

    fix: siteBar data for index (#45)

--- apps/frontend/src/gql/gql.meta.ts
+++ apps/frontend/src/gql/gql.meta.ts
@@ -1,8 +1,26 @@
 import { gql } from '@apollo/client';
 
 
-export const GET_ARICLES_META = gql`
-  query ArticlesMeta($type: String!, $tag: String!) {
+export const GET_ARICLES_META_FILTRTYPE_TYPE = gql`
+  query ArticlesMetaFiltrType($type: String!) {
+    articles(filters: { type: { eq: $type } }) {
+      meta {
+        __typename
+        pagination {
+          page
+          total
+          pageSize
+          pageCount
+          __typename
+        }
+      }
+    }
+  }
+`;
+
+
+export const GET_ARICLES_META_FILTRTYPETAG_TYPE = gql`
+  query ArticlesMetaFiltrTypeTag($type: String!, $tag: String!) {
     articles(filters: { type: { eq: $type }, tags: { title: { eq: $tag } } }) {
       meta {
         __typename
