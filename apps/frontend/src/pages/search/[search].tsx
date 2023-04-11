a7c642baf1188da0c744d84b1c42b84ce2c404f2
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-04-12 01:56:59 +0200

    Development (#66)
    * feat: add nx with next & strapi
    
    * feat: update
    
    * Feat/development submodule utils (#4)
    
    * feat: add submodiles
    
    * fix: delete package-lock.json
    
    * feat: add submodules
    
    * fix: delete backend (#9)
    
    * fix: package.json (#10)
    
    * Frontend (#13)
    
    * fix: package.json
    
    * fix: update git submodule
    
    * update gitsubmodule
    
    * Feat/apolloclient (#16)
    
    * fix: package.json
    
    * fix: update git submodule
    
    * update gitsubmodule
    
    * fix: update gitsubmodule
    
    * feat: migrate
    
    * feat: add basic config for apollo client
    
    * Feat/apolloclient (#19)
    
    * fix: package.json
    
    * fix: update git submodule
    
    * update gitsubmodule
    
    * fix: update gitsubmodule
    
    * feat: migrate
    
    * feat: add basic config for apollo client
    
    * feat: add provider
    
    * feat: add query apollo client
    
    * fix: fix import
    
    * fix: fix submodule (#22)
    
    * fix: fix production ts (#25)
    
    * feat: add hooks
    
    * Feat/data search (#32)
    
    * feat: add hook for data search
    
    * feat: add hooks
    
    * fix: fix ts
    
    * Feat/data search (#34)
    
    * feat: add hook for data search
    
    * feat: add hooks
    
    * fix: fix ts
    
    * fix: fix hook
    
    * fix: update search hooks
    
    * feat: update
    
    * feat: update prettier
    
    * Feat/site bar (#38)
    
    * feat: update next.config
    
    * feat: add siteBar
    
    * Feat/data for site bar (#41)
    
    * feat: add logic for data siteBar
    
    * feat: update uxu-frontend-component
    
    * feat: update uxu-frontend-component
    
    * fix: siteBar data for index (#45)
    
    * feat: add listingArticle (#48)
    
    * feat: add full article (#51)
    
    * feat: add service (#54)
    
    * Feat/add service (#56)
    
    * feat: add service
    
    * fix: change version uxu-utils
    
    * fix: add views for tag (#59)
    
    * Feat/add view search (#62)
    
    * fix: search hook fix
    
    * feat: add view for search
    
    * fix: change data for img (#65)

--- apps/frontend/src/pages/search/[search].tsx
+++ apps/frontend/src/pages/search/[search].tsx
@@ -1,21 +1,10 @@
 import { Layout } from 'layout';
 import { client } from '../../config';
-import {
-  GET_ARICLES_META_FILTRTYPE_TYPE,
-  GET_ARICLES_META_FILTRTYPETAG_TYPE,
-  GET_LISTING_ARTICLES_META_TYPE,
-  GET_SEARCH,
-  GET_SEARCH_TYPE,
-  GET_SETTING_PAGE,
-  GET_SETTING_PAGE_TYPE,
-} from '../../gql';
+import { GET_ARICLES_META_FILTRTYPE_TYPE, GET_ARICLES_META_FILTRTYPETAG_TYPE, GET_LISTING_ARTICLES_META_TYPE, GET_SEARCH, GET_SEARCH_TYPE, GET_SETTING_PAGE, GET_SETTING_PAGE_TYPE } from '../../gql';
 import { createSlugForType } from '../../utils';
 import { NextSeoProps } from 'next-seo';
-import type {
-  SpecialProps as SiteBarType,
-} from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/component.siteBar.props';
+import type { SpecialProps as SiteBarType } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/component.siteBar.props';
 import { ArticleShortDataType, createSlug, SectionListingArticles } from 'uxu-utils';
-
 
 export async function getServerSideProps(context) {
   const { search } = context.query;
@@ -36,33 +25,33 @@ export async function getServerSideProps(context) {
     variables: { query: search },
   });
 
-
-  data.search = searchType?.data?.search?.articles?.data ? searchType?.data?.search?.articles?.data?.map((art) => ({
-      content: {
-        id: art.id,
-        title: art.attributes.title,
-        slug: `${createSlugForType(art.attributes.type)}/${createSlug(art.attributes.title)}-${art.id}`,
-        createdAt: art.attributes.createdAt,
-        author: {
-          name: art.attributes?.author.data.attributes.username || 'autor',
-          avatar: {
-            src: art.attributes?.author.data.attributes.avatar.data.attributes.formats.thumbnail.url,
-            alt: art.attributes?.author.data.attributes.avatar.data.attributes.alternativeText,
+  data.search = searchType?.data?.search?.articles?.data
+    ? searchType?.data?.search?.articles?.data?.map(art => ({
+        content: {
+          id: art.id,
+          title: art.attributes.title,
+          slug: `${createSlugForType(art.attributes.type)}/${createSlug(art.attributes.title)}-${art.id}`,
+          createdAt: art.attributes.createdAt,
+          author: {
+            name: art.attributes?.author.data.attributes.username || 'autor',
+            avatar: {
+              src: art.attributes?.author.data.attributes.avatar.data.attributes.formats.thumbnail.url,
+              alt: art.attributes?.author.data.attributes.avatar.data.attributes.alternativeText,
+            },
           },
+          cover: {
+            src: art?.attributes?.cover?.data?.attributes?.formats?.medium?.url || null,
+            alt: art?.attributes?.cover?.data?.attributes?.alternativeText || null,
+          },
+          tags:
+            art?.attributes?.tags?.data?.map(tag => ({
+              title: tag.attributes.title,
+              slug: `${createSlugForType(`tag`)}/${createSlug(tag.attributes.title)}-${tag.id}`,
+            })) || [],
+          stats: { ratings: 0, comments: 0, views: 0 },
         },
-        cover: {
-          src: art?.attributes?.cover?.data?.attributes?.formats?.large?.url || null,
-          alt: art?.attributes?.cover?.data?.attributes?.alternativeText || null,
-        },
-        tags: art?.attributes?.tags?.data?.map((tag) => ({
-          title: tag.attributes.title,
-          slug: `${createSlugForType(`tag`)}/${createSlug(tag.attributes.title)}-${tag.id}`,
-        })) || [],
-        stats: { ratings: 0, comments: 0, views: 0 },
-      },
-    }
-  )) : [];
-
+      }))
+    : [];
 
   const querySettings = await client.query<GET_SETTING_PAGE_TYPE>({
     query: GET_SETTING_PAGE,
@@ -70,7 +59,7 @@ export async function getServerSideProps(context) {
   });
 
   const attributes = querySettings?.data?.setting?.data?.attributes;
-  data.siteBar.socialMedia.list = attributes?.socialMedia?.map((item) => ({ typ: item.typ, url: item.url }));
+  data.siteBar.socialMedia.list = attributes?.socialMedia?.map(item => ({ typ: item.typ, url: item.url }));
 
   const filter = attributes?.settingsPages[0]?.filter;
   if (filter) {
@@ -83,36 +72,31 @@ export async function getServerSideProps(context) {
       });
 
       const score = countArticle?.data?.articles?.meta?.pagination?.total;
-
-      score && data?.siteBar?.filter?.links?.push({
-        title,
-        score,
-        active: slug === '/',
-        slug: slug === '/' ? slug : `${createSlugForType('tag')}/${slug}${key ? `-${key}` : ''}`,
-      });
+      score &&
+        data?.siteBar?.filter?.links?.push({
+          title,
+          score,
+          active: slug === '/',
+          slug: slug === '/' ? slug : `${createSlugForType('tag')}/${slug}${key ? `-${key}` : ''}`,
+        });
     }
   }
-
 
   return {
     props: { ...data }, // will be passed to the page component as props
   };
 }
 
-
 type Props = {
-  seo: NextSeoProps,
-  siteBar: SiteBarType,
-  search: ArticleShortDataType[]
-}
+  seo: NextSeoProps;
+  siteBar: SiteBarType;
+  search: ArticleShortDataType[];
+};
 
 export default function Search({ seo, siteBar, search }: Props) {
-
   return (
     <Layout seo={seo} siteBar={siteBar}>
       <SectionListingArticles data={search} isLoading={false} />
     </Layout>
   );
 }
-
-
