369b64937afe630e7a727771990a8143f96a50d2
Paweł Niedźwiecki <hello@uxu.pl>
2023-05-09 23:39:52 +0200

    feat: update layout

--- apps/frontend/src/layout/default/layout.default.tsx
+++ apps/frontend/src/layout/default/layout.default.tsx
@@ -2,17 +2,18 @@ import { Box, contents, Footer, Grid, Header, SiteBarPrimary, SiteBarSecondary, 
 import { Home } from 'react-feather';
 import { useHookSearch } from 'hooks';
 import { useRouter } from 'next/router';
-import { Props } from './layout.default.types';
 import { NextSeo } from 'next-seo';
+import { WrapperAlert } from './../layout.global.styles';
+import { ComponentLayoutDataType } from './../layout.global.types';
 
-export const LayoutDefault: Props = ({ siteBarPrimary, siteBarSecondary, dataFooter, seo, alert, children }) => {
+export const LayoutDefault: ComponentLayoutDataType = ({ siteBarPrimary, siteBarSecondary, dataFooter, seo, alert, children }) => {
   const { asPath } = useRouter();
   const { setQuery, res } = useHookSearch();
   const { isDesktopOrLaptop } = useBreakpoints();
   return (
     <>
       <NextSeo {...seo} />
-      <Header alert={alert} res={res} tabs={[{ title: 'Home', value: '/', icon: <Home />, active: asPath === '/' }]} callBack={(search: string) => setQuery(search)} />
+      <Header ComponentAaboveHeader={alert && <WrapperAlert {...alert}/>} res={res} tabs={[{ title: 'Home', value: '/', icon: <Home />, active: asPath === '/' }]} callBack={(search: string) => setQuery(search)} />
       <Grid gridTemplateColumns={{ xs: '1fr', m: '250px 1fr', l: '250px 1fr 250px' }} style={{ maxWidth: contents.maxWidth, margin: '0 auto' }} container>
         <SiteBarPrimary {...siteBarPrimary} />
         <Box position="relative">{children}</Box>
---
+++ apps/frontend/src/layout/layout.global.types.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import { NextSeoProps } from 'next-seo';
+import type { FunctionComponentDiv } from 'uxu-utils';
+import type { SiteBarPrimaryDataTypes } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/primary/component.siteBar.primary.types';
+import type { SiteBarSecondaryDataTypes } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/secondary/component.siteBar.types';
+import type { FooterDataType } from 'uxu-utils/libs/design-system/src/lib/components/templates/footer/component.footer.types';
+
+export type SpecialProps = {
+  siteBarSecondary?: SiteBarSecondaryDataTypes;
+  siteBarPrimary?: SiteBarPrimaryDataTypes;
+  seo?: NextSeoProps;
+  dataFooter: FooterDataType;
+  alert?: {
+    tel?: string;
+    title: string;
+  };
+};
+
+export type ComponentLayoutDataType = FC<FunctionComponentDiv & SpecialProps>;
