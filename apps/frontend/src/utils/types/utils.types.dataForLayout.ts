0663129916338fa52b535a6a3163211451ccbc84
Paweł Niedźwiecki <hello@uxu.pl>
2023-05-10 00:09:49 +0200

    feat: update uxu-utils

--- apps/frontend/src/utils/types/utils.types.dataForLayout.ts
+++ apps/frontend/src/utils/types/utils.types.dataForLayout.ts
@@ -1,11 +1,11 @@
 import { NextSeoProps } from 'next-seo';
 import { FooterDataType } from 'uxu-utils/libs/design-system/src/lib/components/templates/footer/component.footer.types';
-import { SpecialProps as SiteBarPrimaryType } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/primary/component.siteBar.primary.types';
-import { SpecialProps as SiteBarSecondaryType } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/secondary/component.siteBar.types';
+import { SiteBarPrimaryDataTypes } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/primary/component.siteBar.primary.types';
+import { SiteBarSecondaryDataTypes } from 'uxu-utils/libs/design-system/src/lib/components/templates/siteBar/secondary/component.siteBar.types';
 
 export type DataForLayout = {
   seo: NextSeoProps;
   dataFooter: FooterDataType;
-  siteBarPrimary: SiteBarPrimaryType;
-  siteBarSecondary: SiteBarSecondaryType;
+  siteBarPrimary: SiteBarPrimaryDataTypes;
+  siteBarSecondary: SiteBarSecondaryDataTypes;
 };
