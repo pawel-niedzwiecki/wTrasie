26d836fcb3369030d101fe9af69066cce0c813e1
Paweł Niedźwiecki <hello@uxu.pl>
2023-05-11 23:38:44 +0200

    feat: add page with service

--- apps/frontend/src/layout/default/layout.default.tsx
+++ apps/frontend/src/layout/default/layout.default.tsx
@@ -1,25 +1,41 @@
 import { Box, contents, Footer, Grid, Header, SiteBarPrimary, SiteBarSecondary, useBreakpoints } from 'uxu-utils';
-import { Home } from 'react-feather';
+import { Home, Tool } from 'react-feather';
 import { useHookSearch } from 'hooks';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 import { WrapperAlert } from './../layout.global.styles';
 import { ComponentLayoutDataType } from './../layout.global.types';
 
-export const LayoutDefault: ComponentLayoutDataType = ({ siteBarPrimary, siteBarSecondary, dataFooter, seo, alert, children }) => {
-  const { asPath } = useRouter();
-  const { setQuery, res } = useHookSearch();
-  const { isDesktopOrLaptop } = useBreakpoints();
+export const LayoutDefault: ComponentLayoutDataType = ( {
+                                                          siteBarPrimary,
+                                                          siteBarSecondary,
+                                                          dataFooter,
+                                                          seo,
+                                                          alert,
+                                                          children
+                                                        } ) => {
+  const {asPath} = useRouter ();
+  const {setQuery, res} = useHookSearch ();
+  const {isDesktopOrLaptop} = useBreakpoints ();
   return (
     <>
       <NextSeo {...seo} />
-      <Header ComponentAaboveHeader={alert && <WrapperAlert {...alert}/>} res={res} tabs={[{ title: 'Home', value: '/', icon: <Home />, active: asPath === '/' }]} callBack={(search: string) => setQuery(search)} />
-      <Grid gridTemplateColumns={{ xs: '1fr', m: '250px 1fr', l: '250px 1fr 250px' }} style={{ maxWidth: contents.maxWidth, margin: '0 auto' }} container>
+      <Header ComponentAaboveHeader={alert && <WrapperAlert {...alert}/>} res={res}
+              tabs={[
+                {title: 'Home', value: '/', icon: <Home/>, active: asPath === '/'},
+                {
+                  title: 'Usługi',
+                  value: '/s',
+                  icon: <Tool/>,
+                  active: asPath === '/s'
+                }]} callBack={( search: string ) => setQuery ( search )}/>
+      <Grid gridTemplateColumns={{xs: '1fr', m: '250px 1fr', l: '250px 1fr 250px'}}
+            style={{maxWidth: contents.maxWidth, margin: '0 auto'}} container>
         <SiteBarPrimary {...siteBarPrimary} />
         <Box position="relative">{children}</Box>
         {isDesktopOrLaptop && <SiteBarSecondary {...siteBarSecondary} />}
       </Grid>
-      <Footer columns={dataFooter?.columns} />
+      <Footer columns={dataFooter?.columns}/>
     </>
   );
 };
---
+++ apps/frontend/src/pages/s/index.tsx
@@ -0,0 +1,41 @@
+import { LayoutDefault } from 'layout';
+import {
+  DataForLayout,
+  DataForSectionListingArticles,
+  ParserDataFromApiGetArticleListToArticlesListData,
+  ParserDataFromGetSettingApiToLayoutData
+} from 'utils';
+import { SectionListingArticles } from 'uxu-utils';
+import { clientGetArticlesListQuery, clientGetSettingPageQuery } from 'gql';
+
+type Props = {
+  dataForLayout: DataForLayout;
+  dataForSectionListingArticles: DataForSectionListingArticles;
+};
+
+function Index ( {dataForLayout, dataForSectionListingArticles}: Props ) {
+  return (
+    <LayoutDefault {...dataForLayout}>
+      <SectionListingArticles {...dataForSectionListingArticles} />
+    </LayoutDefault>
+  );
+}
+
+export async function getServerSideProps () {
+  // set data for LayoutDefault
+  const querySettings = await clientGetSettingPageQuery ( {page: 'home'} );
+  const dataForLayout: DataForLayout = new ParserDataFromGetSettingApiToLayoutData ( {
+    data: querySettings.data,
+    slug: '/',
+  } ).getData ();
+
+  // set data for SectionListingArticles
+  const articlesList = await clientGetArticlesListQuery ( {page: 1, type: ['service']} );
+  const dataForSectionListingArticles: DataForSectionListingArticles = new ParserDataFromApiGetArticleListToArticlesListData ( {getArticlesList: articlesList.data} ).getData ();
+
+  return {
+    props: {dataForSectionListingArticles, dataForLayout},
+  };
+}
+
+export default Index;
