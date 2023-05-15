edcbe386c98d142adb3d6083f84e473dca0504ee
Paweł Niedźwiecki <hello@uxu.pl>
2023-05-15 14:26:36 +0200

    feat: add icon to alert

--- apps/frontend/src/layout/default/layout.default.tsx
+++ apps/frontend/src/layout/default/layout.default.tsx
@@ -6,29 +6,39 @@ import { NextSeo } from 'next-seo';
 import { WrapperAlert } from './../layout.global.styles';
 import { ComponentLayoutDataType } from './../layout.global.types';
 
-export const LayoutDefault: ComponentLayoutDataType = ( {
-                                                          siteBarPrimary,
-                                                          siteBarSecondary,
-                                                          dataFooter,
-                                                          seo,
-                                                          alert,
-                                                          children
-                                                        } ) => {
+export const LayoutDefault: ComponentLayoutDataType = (
+  {
+    siteBarPrimary,
+    siteBarSecondary,
+    dataFooter,
+    seo,
+    alert,
+    children
+  } ) => {
   const {asPath} = useRouter ();
   const {setQuery, res} = useHookSearch ();
   const {isDesktopOrLaptop} = useBreakpoints ();
+
+  const tabs = [
+    {
+      title: 'Home',
+      value: '/',
+      icon: <Home/>,
+      active: asPath === '/'
+    },
+    {
+      title: 'Usługi',
+      value: '/s',
+      icon: <Tool/>,
+      active: asPath === '/s'
+    }
+  ];
+
   return (
     <>
       <NextSeo {...seo} />
-      <Header ComponentAaboveHeader={alert && <WrapperAlert {...alert}/>} res={res}
-              tabs={[
-                {title: 'Home', value: '/', icon: <Home/>, active: asPath === '/'},
-                {
-                  title: 'Usługi',
-                  value: '/s',
-                  icon: <Tool/>,
-                  active: asPath === '/s'
-                }]} callBack={( search: string ) => setQuery ( search )}/>
+      <Header ComponentAaboveHeader={alert && <WrapperAlert {...alert} />} res={res}
+              tabs={tabs} callBack={( search: string ) => setQuery ( search )}/>
       <Grid gridTemplateColumns={{xs: '1fr', m: '250px 1fr', l: '250px 1fr 250px'}}
             style={{maxWidth: contents.maxWidth, margin: '0 auto'}} container>
         <SiteBarPrimary {...siteBarPrimary} />
