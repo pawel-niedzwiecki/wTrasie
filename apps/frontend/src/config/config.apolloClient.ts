fd14dc46dc12393c7b8c1cca65552bdeedb61acc
Paweł Niedźwiecki <37551231+pawel-niedzwiecki@users.noreply.github.com>
2023-06-10 00:09:38 +0200

    Refactor/sitemap.xml (#142)
    * refactor: sitemap.xml
    
    * fix: parser fix

--- apps/frontend/src/config/config.apolloClient.ts
+++ apps/frontend/src/config/config.apolloClient.ts
@@ -6,4 +6,10 @@ export const client = new ApolloClient({
   ssrMode: true,
   uri: `${api_url}/graphql`,
   cache,
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  }
 });
