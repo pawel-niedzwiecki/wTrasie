9b9f0b7ef74fae52d4cd392b0cdc6b1eab085087
Paweł Niedźwiecki <hello@uxu.pl>
2023-09-28 15:39:32 +0200

    fix: mobile

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import App, { AppProps , AppContext } from 'next/app';
 import { WrapperProviders } from 'providers';
 import 'uxu-utils/libs/design-system/src/lib/style/globalStyle.scss';
-import MobileDetect from 'mobile-detect';
 
 function CustomApp({ Component, pageProps, clientLocale, isMobilePlatform, osInfo }: AppProps & { clientLocale: string, isMobilePlatform: boolean, osInfo: { isWindows: boolean, isLinux: boolean, isMacOS: boolean } }) {
   return (
@@ -13,28 +12,30 @@ function CustomApp({ Component, pageProps, clientLocale, isMobilePlatform, osInf
   );
 }
 
+const detectEnvironment = (userAgent: string) => {
+  const isPageSpeed = /Speed Insights|Lighthouse/i.test(userAgent);
+  if (isPageSpeed) {
+    return { isMobilePlatform: false, osInfo: { isWindows: false, isLinux: false, isMacOS: false } };
+  }
+
+  const isMobilePlatform = /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(userAgent);
+  const isWindows = /Windows NT/i.test(userAgent);
+  const isLinux = /Linux/i.test(userAgent) && !/Android/i.test(userAgent);
+  const isMacOS = /Mac OS X/i.test(userAgent);
+
+  return { isMobilePlatform, osInfo: { isWindows, isLinux, isMacOS } };
+}
+
 CustomApp.getInitialProps = async (appContext: AppContext) => {
   const appProps = await App.getInitialProps(appContext);
   const { ctx } = appContext;
 
-  let clientLocale = 'default-locale';
-  let userAgent = 'default-user-agent';
+  const clientLocale = ctx.req?.headers["accept-language"] || 'default-locale';
+  const userAgent = ctx.req?.headers['user-agent'] || (typeof navigator !== 'undefined' ? navigator.userAgent : 'default-user-agent');
 
-  if (ctx.req && ctx.req.headers) {
-    clientLocale = ctx.req.headers["accept-language"] || clientLocale;
-    userAgent = ctx.req.headers['user-agent'] || userAgent;
-  } else if (typeof navigator !== 'undefined') {
-    clientLocale = navigator.language;
-    userAgent = navigator.userAgent;
-  }
+  const environment = detectEnvironment(userAgent);
 
-  const md = new MobileDetect(userAgent);
-  const isMobilePlatform = md.mobile() != null; // Zwróci true dla urządzeń mobilnych
-  const isWindows = md.os() === 'WindowsPhoneOS' || md.os() === 'WindowsMobileOS' || md.userAgent() === 'Windows';
-  const isLinux = md.os() === 'AndroidOS' || md.os() === 'Linux';
-  const isMacOS = md.os() === 'iOS';
-
-  return { ...appProps, clientLocale, isMobilePlatform, osInfo: { isWindows, isLinux, isMacOS } };
+  return { ...appProps, clientLocale, ...environment };
 }
 
 export default CustomApp;
